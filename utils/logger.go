bd3011a3138f1c638ee35c3b75741e45c6a237ad
Sergey Shevchenko <sergeyshevchdevelop@gmail.com>
2022-07-30 16:11:45 +0300

    Ignore error on log sync

--- utils/logger.go
+++ utils/logger.go
@@ -1,23 +1,15 @@
 package utils
 
 import (
-	"errors"
-	"syscall"
-
 	"go.uber.org/zap"
 )
 
-func InitLogger() (*zap.Logger, func(logger *zap.Logger)) {
+func InitLogger() *zap.Logger {
 	log, err := zap.NewDevelopment()
 	if err != nil {
 		panic(err)
 	}
 	log = log.WithOptions(zap.AddStacktrace(zap.PanicLevel))
 
-	return log, func(log *zap.Logger) {
-		err := log.Sync()
-		if err != nil && !errors.Is(err, syscall.ENOTTY) {
-			panic(err)
-		}
-	}
+	return log
 }
